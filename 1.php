23013f201ac37b4fc4f20658e29a692ed9514738
aevstratov <alevure@yandex.ru>
2021-06-02 10:33:49 +0400

    task 2,3

--- 1.php
+++ 1.php
@@ -1,4 +1,13 @@
-<html>
+<?php
+
+$name = 'Михаил';
+$residentialAddress = 'Москва улица Ленина 7';
+$homeAddress = 'Лондон Бейкер Стрит 221B';
+$age = 57;
+$sex = 'мужской';
+
+?>
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -7,7 +16,9 @@
     <title>Document</title>
 </head>
 <body>
-
+<?=
+"Меня зовут $name, мне $age лет, мой пол $sex, я проживаю по адресу $homeAddress, а прописан $residentialAddress"
+?>
 </body>
 </html>
-<?php
+
