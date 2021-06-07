be2f73a64ed4970ca4b9d6f263a8baf47d7c0958
aevstratov <alevure@yandex.ru>
2021-06-07 16:39:17 +0400

    1-3 task

---
+++ lesson2/1.php
@@ -0,0 +1,23 @@
+<html lang="RU">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $numberOne = 5;
+    $numberTwo = 10;
+    $sum = $numberOne + $numberTwo;
+    $dif = $numberOne - $numberTwo;
+    $div = $numberOne / $numberTwo;
+    $multi = $numberOne * $numberTwo;
+    $expo = pow($numberOne, $numberTwo) ;
+    echo "$numberOne + $numberTwo = $sum - сложение<br>";
+    echo "$numberOne - $numberTwo = $dif - вычитание<br>";
+    echo "$numberOne / $numberTwo = $div - деление<br>";
+    echo "$numberOne * $numberTwo = $multi - умножение<br>";
+    echo "$numberOne<sup>$numberTwo</sup> = $expo - возведение в степень<br>";
+    ?>
+</body>
+</html>
+
---
+++ lesson2/2.php
@@ -0,0 +1,18 @@
+<html lang="RU">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $str = '456';
+    var_dump($str);
+    echo "<br>";
+    settype($str, "int");
+    var_dump($str);
+    
+    ?>
+</body>
+</html>
+
