7689cfafddb2e9670bfe9a57cbbc2ecf8e596f6b
aevstratov <alevure@yandex.ru>
2021-07-14 09:56:17 +0400

    task 1-2

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+function get_page () {
+    if (!empty($_GET['page'])) {
+        switch ($_GET['page']) {
+            case 2:
+                include '2.php';
+                break;
+            case 3:
+                include '3.php';
+                break;
+            default:
+                include '1.php';
+                break;
+        }
+    }
+}
+
+get_page();
+?>
+
+<a href="?page=1">Ссылка 1</a>
+<a href="?page=2">Ссылка 2</a>
+<a href="?page=3">Ссылка 3</a>
